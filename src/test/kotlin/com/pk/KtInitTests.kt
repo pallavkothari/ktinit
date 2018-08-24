10ea5406ce26a528b939554109fa0dc548d0774c
Pallav Kothari <pallavkothari@gmail.com>
2018-08-24 12:26:32 -0700

    mustache test

--- src/test/kotlin/com/pk/KtInitTests.kt
+++ src/test/kotlin/com/pk/KtInitTests.kt
@@ -1,11 +1,23 @@
 package com.pk
 
+import com.github.mustachejava.DefaultMustacheFactory
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import java.io.StringReader
+import java.io.StringWriter
 
 class KtInitTests {
 
 	@Test
 	fun contextLoads() {
+		val template = StringReader("Hello {{name}}!")
+        val ctx = mapOf("name" to "world")
+        val mf = DefaultMustacheFactory()
+        val mustache = mf.compile(template, "template")
+        val writer = StringWriter()
+        mustache.execute(writer, ctx)
+        writer.flush()
+        assertThat(writer.toString()).isEqualTo("Hello world!")
 	}
 
 }
