9dd2b365c9e08dad52d82c19e17ece156354994a
Pallav Kothari <pallavkothari@gmail.com>
2018-08-23 23:20:01 -0700

    init

---
+++ src/main/kotlin/com/pk/KtinitApplication.kt
@@ -0,0 +1,11 @@
+package com.pk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtinitApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtinitApplication>(*args)
+}
